7374cd1f96e42a51eb94823d3226ef0484453c94
collozy <ozlocollins@gmail.com>
2019-07-04 18:45:19 +0300

    Added vote controls

--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -5,31 +5,55 @@
     <div class="row justify-content-center">
         <div class="col-md-12">
             <div class="card">
-                <div class="card-header">
-                    <div class="d-flex align-items-center">
-                        <h2>{{ $question->title }}</h2>
-                        <div class="ml-auto">
-                            <a href="{{ route('questions.index') }}" class="btn btn-outline-secondary">Back to all questions</a>
-                        </div>
-                    </div>
-                </div>
-
                 <div class="card-body">
-                    {{-- {{ $question->body }} --}}
-                    {!! $question->body_html !!}
-                    <div class="float-right">
-                        <span class="text-muted">Answered {{ $question->created_date }}</span>
-                        <div class="media mt-2">
-                            <a href="{{ $question->user->url }}" class="pr-2">
-                                <img src="{{ $question->user->avatar }}" alt="User Avatar">
-                            </a>
-                            <div class="media-body mt-1">
-                                <a href="{{ $question->user->url }}">{{ $question->user->name }}</a>
+                        <div class="card-title">
+                                <div class="d-flex align-items-center">
+                                    <h2>{{ $question->title }}</h2>
+                                    <div class="ml-auto">
+                                        <a href="{{ route('questions.index') }}" class="btn btn-outline-secondary">Back to all questions</a>
+                                    </div>
+                                </div>
+                        </div>
+
+                        <hr>
+
+
+                        <div class="media">
+                            <div class="d-flex flex-column vote-controls">
+                                <a href="" title="This quiz is helpful" class="vote-up">
+                                     {{-- <i class="fas fa-caret-up fa-3x"></i> --}}
+                                    <img src="/svg/open-iconic/svg/caret-top.svg" alt="icon name">
+                                </a>
+                                <span class="votes-count">12230</span>
+                                <a href="" title="This quiz doesn't help" class="vote-down off">
+                                    {{-- <i class="fas fa-caret-down fa-3x"></i> --}}
+                                    <img src="/svg/open-iconic/svg/caret-bottom.svg" alt="icon name">
+                                </a>
+                                <a href="" title="Mark as favorite" class="favorite mt-2">
+                                    {{-- <i class="fas fa-star fa-3x"></i> fontawesome doesn't work --}}
+                                    <img src="/svg/open-iconic/svg/star.svg" alt="icon name">
+                                    <span class="favorites-count">123</span>
+                                </a>
+                            </div>
+                            <div class="media-body">
+                                {{-- {{ $question->body }} --}}
+                                {!! $question->body_html !!}
+                                <div class="float-right">
+                                    <span class="text-muted">Answered {{ $question->created_date }}</span>
+                                    <div class="media mt-2">
+                                        <a href="{{ $question->user->url }}" class="pr-2">
+                                            <img src="{{ $question->user->avatar }}" alt="User Avatar">
+                                        </a>
+                                        <div class="media-body mt-1">
+                                            <a href="{{ $question->user->url }}">{{ $question->user->name }}</a>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
                         </div>
-                    </div>
 
                 </div>
+
             </div>
         </div>
     </div>
@@ -43,6 +67,21 @@
                     <hr>
                     @foreach ($question->answers as $answer)
                         <div class="media">
+                            <div class="d-flex flex-column vote-controls">
+                                <a href="" title="This answer is helpful" class="vote-up">
+                                     {{-- <i class="fas fa-caret-up fa-3x"></i> --}}
+                                    <img src="/svg/open-iconic/svg/caret-top.svg" alt="icon name">
+                                </a>
+                                <span class="votes-count">12230</span>
+                                <a href="" title="This answer doesn't help" class="vote-down off">
+                                    {{-- <i class="fas fa-caret-down fa-3x"></i> --}}
+                                    <img src="/svg/open-iconic/svg/caret-bottom.svg" alt="icon name">
+                                </a>
+                                <a href="" title="Mark as best" class="vote-accepted mt-2">
+                                    {{-- <i class="fas fa-star fa-3x"></i> fontawesome doesn't work --}}
+                                    <img src="/svg/open-iconic/svg/check.svg" alt="icon name">
+                                </a>
+                            </div>
                             <div class="media-body">
                                 {!! $answer->body_html !!}
                                 <div class="float-right">
