ae24c92da2c77f39aee5ba6964787bf7c83e08fe
collozy <ozlocollins@gmail.com>
2019-07-10 11:17:31 +0300

    Refactored models(Quiz,Ans,User) using DRY principle

--- app/Question.php
+++ app/Question.php
@@ -7,6 +7,7 @@ use Illuminate\Foundation\Auth\User;
 
 class Question extends Model
 {
+    use VotableTrait;
     protected $fillable = ['title', 'body'];
 
     public function user()
@@ -74,20 +75,5 @@ class Question extends Model
     public function getFavoritesCountAttribute()
     {
         return $this->favorites->count();
-    }
-
-    public function votes()
-    {
-        return $this->morphToMany(User::class, 'votable');
-    }
-
-    public function upVotes()
-    {
-        return $this->votes()->wherePivot('vote', 1);
-    }
-
-    public function downVotes()
-    {
-        return $this->votes()->wherePivot('vote', -1);
     }
 }
